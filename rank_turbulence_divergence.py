553aa8a87fc0b0323bca88944028a9aac0feab3f
PhiCtl <philippine.descourtils@epfl.ch>
2023-04-19 11:07:19 +0200

    Came up with script for 3 years consecutive months divergence computations

--- rank_turbulence_divergence.py
+++ rank_turbulence_divergence.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 from tqdm import tqdm
 
 import os
@@ -7,10 +9,10 @@ import pyspark
 from pyspark.sql import *
 from pyspark.sql.functions import *
 
-from ranking_helpers import compute_ranks_bins
+from ranking_helpers import compute_ranks_bins, rank_turbulence_divergence_sp
 
 conf = pyspark.SparkConf().setMaster("local[*]").setAll([
-                                   ('spark.driver.memory','24g'),
+                                   ('spark.driver.memory','42G'),
                                    ('spark.driver.maxResultSize', '8G')
                                   ])
 # create the session
@@ -18,53 +20,36 @@ spark = SparkSession.builder.config(conf=conf).getOrCreate()
 # create the context
 sc = spark.sparkContext
 
+def custom_join(df1, df2):
 
-def rank_turbulence_divergence_sp(rks, d1, d2, N1, N2, alpha):
-    """
-    Compute rank turbulence divergence between date d1 and d2 for pages in rks
-    :param rks: dataframe with columns d1 and d2
-    :param d1: string date 1 in format YYYY-MM
-    :param d2: string date 2 in format YYYY-MM
-    :param N1: number of elements at d1
-    :param N2: number of elements at d2
-    :param alpha: hyper parameter for divergence computation
-    """
-    computations = rks.select(d1, d2, d1 + '_nn', d2 + '_nn', 'page')
-    tmp_1 = computations.where(~col(d1).isNull()).withColumn('1/d1**alpha_N', pow(lit(1) / col(d1), lit(alpha)))
-    tmp_2 = computations.where(~col(d2).isNull()).withColumn('1/d2**alpha_N', pow(lit(1) / col(d2), lit(alpha)))
-    tmp_1 = tmp_1.withColumn('diff_N_1',
-                             pow(abs(col('1/d1**alpha_N') - lit((1 / (N1 + 0.5 * N2)) ** alpha)), lit(1 / (alpha + 1))))
-    tmp_2 = tmp_2.withColumn('diff_N_2',
-                             pow(abs(col('1/d2**alpha_N') - lit((1 / (N2 + 0.5 * N1)) ** alpha)), lit(1 / (alpha + 1))))
-    N = (alpha + 1) / alpha * (tmp_1.select(sum('diff_N_1').alias('dn1')).collect()[0][0] +
-                               tmp_2.select(sum('diff_N_2').alias('dn2')).collect()[0][0])
-
-    computations = computations.withColumn('1/d1**alpha', pow(lit(1) / col(d1 + '_nn'), lit(alpha)))
-    computations = computations.withColumn('1/d2**alpha', pow(lit(1) / col(d2 + '_nn'), lit(alpha)))
-    computations = computations.withColumn(f'div_{d2}',
-                                           pow(abs(col('1/d1**alpha') - col('1/d2**alpha')), lit(1 / (alpha + 1))) * (
-                                                       alpha + 1) / (alpha * N))
-
-    return computations.select('page', f'div_{d2}')
+    return df1.join(df2, "page", how='outer')
 
 if __name__ == '__main__':
 
-    # Data
-    dfs = spark.read.parquet("/scratch/descourt/processed_data/pageviews_agg_en.wikipedia_2019_2020_2021_2022.parquet")
-    dfs = dfs.filter(~dfs.page.contains(":"))
-
-
-    path = "/home/descourt/interm_results/low_alpha_log_all"
+    # path
+    path = '/scratch/descourt/interm_results/rank_div'
     os.makedirs(path, exist_ok=True)
-    slicing = 1000
-    lim = int(1e6)
-    df_ranks = compute_ranks_bins(dfs, slicing=slicing, lim=lim, log=True)
 
-    df_divs = None
+    # Data - 2019-2020-2021
+    dfs = spark.read.parquet("/scratch/descourt/processed_data/pageviews_agg_en.wikipedia_2019_2020_2021_2022.parquet")
+    dfs = dfs.filter(~dfs.page.contains(":") & (dfs.tot_count_views >= 1) & (dfs.date.contains(['2019', '2020', '2021'])))
 
-    # 1. consider date per date
-    months = months = [str(m + 1) if (m + 1) / 10 >= 1 else f"0{m + 1}" for m in range(12)]
-    dates = [f"{y}-{m}" for y in ['2019', '2020'] for m in months]
+    # Extract high volume core
+    df_rank = compute_ranks_bins(dfs, slicing=1000, lim=2 * int(1e7))
+    window = Window.partitionBy('date').orderBy('rank')
+    df_cutoff = df_rank.withColumn('cum_views', sum('tot_count_views').over(window)) \
+        .select(col('date').alias('d'), 'cum_views', 'rank', 'page')
+    df_sum = df_rank.groupBy('date').agg(sum('tot_count_views').alias('tot_count_month'))
+    df_cutoff = df_cutoff.join(df_sum, df_sum.date == df_cutoff.d) \
+        .withColumn('perc_views', col('cum_views') / col('tot_count_month') * 100) \
+        .drop('d')
+    df_high_volume = df_cutoff.where(df_cutoff.perc_views <= 90.75) # Empirical
+
+    # consider date per date
+    months = [str(m + 1) if (m + 1) / 10 >= 1 else f"0{m + 1}" for m in range(12)]
+    dates = [f"{y}-{m}" for y in ['2019', '2020', '2021'] for m in months]
+
+    dfs_divs = []
 
     for i in tqdm(range(len(dates) - 1)):
 
@@ -72,28 +57,26 @@ if __name__ == '__main__':
 
         d1 = dates[i]
         d2 = dates[i + 1]
-        df_ranks_filt = df_ranks.where(df_ranks.date.isin([d1, d2])).groupBy('page').pivot('date').sum('rank')
+        df_ranks_piv = df_high_volume.where(df_high_volume.date.isin([d1, d2]))\
+                                     .groupBy('page').pivot('date').sum('rank')
 
         # 2. select rows for which at least 1 non null in both columns
-        df_filt = df_ranks_filt.where(~col(d2).isNull() | ~col(d1).isNull())
+        df_comparison = df_ranks_piv.where(~col(d2).isNull() | ~col(d1).isNull())
 
         # 3. compute last rank with average formula (extracting number of non null elements in comparison date)
-        N1 = df_filt.where(~col(d1).isNull()).count()
-        N2 = df_filt.where(~col(d2).isNull()).count()
-        N = df_filt.where(~col(d2).isNull() & ~col(d1).isNull()).count()  # Intersection
+        N1 = df_comparison.where(~col(d1).isNull()).count()
+        N2 = df_comparison.where(~col(d2).isNull()).count()
+        N = df_comparison.where(~col(d2).isNull() & ~col(d1).isNull()).count()  # Intersection
 
         last_rk1 = N1 + 0.5 * (N2 - N)
         last_rk2 = N2 + 0.5 * (N1 - N)
-        df_filt = df_filt.withColumn(d1 + '_nn', col(d1)).withColumn(d2 + '_nn', col(d2))
-        df_filt = df_filt.fillna({d1 + '_nn': last_rk1, d2 + '_nn': last_rk2})
+        df_comparison = df_comparison.withColumn(d1 + '_nn', col(d1)).withColumn(d2 + '_nn', col(d2))
+        df_comparison = df_comparison.fillna({d1 + '_nn': last_rk1, d2 + '_nn': last_rk2})
 
         # 4. Do calculations and store result
-        df_filt = rank_turbulence_divergence_sp(df_filt, d1, d2, N1, N2, 0.001)
-        if i == 0:
-            df_divs = df_filt.select('page', f'div_{d2}')
-        else:
-            df_tmp = df_filt.select('page', f'div_{d2}')
-            df_divs = df_divs.join(df_tmp, 'page', 'outer')
-            print(df_divs)
+        dfs_divs.append(rank_turbulence_divergence_sp(df_comparison, d1, d2, N1, N2, 0.0001))
 
-    df_divs.write.parquet(os.path.join(path, f"div_slice_all.parquet"))
+    print("Writing to file...")
+    dfs_final = reduce(custom_join, dfs_divs)
+    dfs_final.write.parquet(os.path.join(path, 'rank_turb_div_all.parquet'))
+    print("Done !")
