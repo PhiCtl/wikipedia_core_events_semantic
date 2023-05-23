96421f602b444685547b582850624d1be4ae6182
PhiCtl <philippine.descourtils@epfl.ch>
2023-05-23 09:41:16 +0200

    Matching missing

--- data_aggregation.py
+++ data_aggregation.py
@@ -368,7 +368,7 @@ def match_missing_ids(dfs=None, df_topics_sp=None, save_interm=True):
 
     print('Load data')
     if dfs is None:
-        dfs = spark.read.parquet("/scratch/descourt/processed_data_052223/pageviews_agg_en_2015-2023.parquet")
+        dfs = spark.read.parquet("/scratch/descourt/processed_data_052223/pageviews_en_2015-2023.parquet")
     if df_topics_sp is None:
         df_topics_sp = spark.read.parquet('/scratch/descourt/topics/topic/topics-enwiki-20230320-parsed.parquet')
 
@@ -438,4 +438,4 @@ def corrected_filtering():
 
 
 if __name__ == '__main__':
-    automated_main()
+    match_missing_ids()
