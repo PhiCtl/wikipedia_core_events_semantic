cf0036a70da2374320edf098602baeaaf0546eef
PhiCtl <philippine.descourtils@epfl.ch>
2023-05-26 16:27:42 +0200

    Corrected function output

--- make_and_plot.py
+++ make_and_plot.py
@@ -480,7 +480,7 @@ if __name__ == '__main__':
 
         for p, n in tqdm(zip(prev_d, next_d)):
 
-            df_ranked, df_heatmap, N1, N2, N = prepare_topic_heatmap(df_topics_sp=df_topics_sp,
+            df_heatmap = prepare_topic_heatmap(df_topics_sp=df_topics_sp,
                                                                      df=dfs,
                                                                      prev_date=p,
                                                                      next_date=n,
