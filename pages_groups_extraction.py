29d0b71f3b7aa11131f6401e859f8a4b3e30ffc8
PhiCtl <philippine.descourtils@epfl.ch>
2023-06-12 15:40:07 +0200

    Cleaned functions and added doc

--- pages_groups_extraction.py
+++ pages_groups_extraction.py
@@ -5,30 +5,17 @@ from pyspark.sql import *
 from pyspark.sql.functions import *
 
 
-
-def find_hinge_point_np(y):
-    # Assumes curve is not noisy
-
-    slope = 100 / len(y)
-    h_idx = np.where(np.gradient(y) <= slope)[0][0]
-    return h_idx / len(y) * 100, y[h_idx]
-
-def plot_volume_curve(df, label):
-    plt.figure()
-    df.plot(x='perc_rank', y='perc_views', label=label)
-    idx_h, h = find_hinge_point_np(df['perc_views'].values)
-    plt.axhline(h, c='g')
-    plt.axvline(idx_h, c='g', label=f'{int(h)}%-{idx_h}% of total rank')
-    plt.title(f'Cumulative views distribution of Wikipedia pages for {label}')
-    plt.xlabel('Percentage of last rank')
-    plt.ylabel('Cumulative percentage of pageviews monthly volume')
-    plt.legend()
-
-
-
 def compute_gradient(df, x):
-    # We do not compute boundaries derivative for each month
-    # We use central finite difference, assuming regular (1) spacing
+
+    """
+    Compute spatial derivative d_x / d_h, with h spatial step of size 1
+    :param df : DataFrame
+    :param x : str, name of the column from which we want to take the 1st order derivative
+
+    Note :
+    - We do not compute boundaries derivative for each month
+    - We use central finite difference, assuming regular (1) spacing
+    """
 
     w = Window.partitionBy('date').orderBy('rank')
     df_h = df.withColumn('p', lag(x).over(w)) \
@@ -40,9 +27,16 @@ def compute_gradient(df, x):
 
 
 def find_hinge_point(df, eps=1):
+    """
+    Find hinge point :
+    The hinge can be defined as the point on the curve where we start adding “more ranks than views”
+    in terms of contribution.
+    :param df : DataFrame
+    :param eps : inclination of the slope tangent to the hinge point. Default = 1
+    """
     df_grad = compute_gradient(df, x='perc_views')
     slopes = df.groupBy('date').agg(count('*').alias('slope')).select(col('date').alias('d'),
-                                                                      (100 / col('slope') * eps).alias('slope'))
+                                                                      (100*eps / col('slope')).alias('slope'))
     df_hinge = df_grad.join(slopes, slopes.d == df_grad.date) \
         .drop('d') \
         .sort(asc('rank')) \
@@ -50,20 +44,32 @@ def find_hinge_point(df, eps=1):
         .groupBy('date').agg(first('perc_views').alias('hinge_perc'), first('perc_rank').alias('hinge_rank'))
     return df_hinge
 
-def compute_volumes(df, partition='date', sampling=1):
+def compute_volumes(df, sampling=1):
+    """
+    Compute the ordered cumulated views and ranks for core / tail extraction using the hinge method
+    :param df: DataFrame (pyspark)
+    :param sampling: int, sampling step of the full dataframe
+    """
 
-    window = Window.partitionBy(partition).orderBy('rank')
+    window = Window.partitionBy('date').orderBy('rank')
     # Sample for robustness tests
     df = df.where((col('rank') % sampling == 0) | (col('rank') == 1))
     df_cutoff = df.withColumn('cum_views', sum('tot_count_views').over(window))
-    df_sum = df.groupBy(partition).agg(sum('tot_count_views').alias('tot_counts'), count('*').alias('tot_nb_pages'))
-    df_cutoff = df_cutoff.join(df_sum, partition) \
+    df_sum = df.groupBy('date').agg(sum('tot_count_views').alias('tot_counts'), count('*').alias('tot_nb_pages'))
+    df_cutoff = df_cutoff.join(df_sum, 'date') \
         .withColumn('perc_views', col('cum_views') / col('tot_counts') * 100) \
         .withColumn('perc_rank', col('rank') / col('tot_nb_pages') * 100).drop('tot_nb_pages', 'tot_counts')
 
     return df_cutoff
 
 def extract_volume(df, high=True, sampling=1, eps=1):
+    """
+    Extract core or tail using the hinge method
+    :param df: dataframe
+    :param high: boolean, whether to extract high or low volume
+    :param sampling: int, sampling step of the full dataframe
+    :param eps: inclination of the slope at the hinge point
+    """
     df_cutoff = compute_volumes(df, sampling=sampling)
     # Find hinge point
     df_hinge = find_hinge_point(df_cutoff, eps=eps).cache()
@@ -75,28 +81,6 @@ def extract_volume(df, high=True, sampling=1, eps=1):
         df_volume = df_cutoff.join(df_hinge, 'date').where(col('perc_views') > col('hinge_perc'))
     return df_volume
 
-
-def extract_common_pages(df, time_period=None, aggregate=False, nb_dates=None):
-    if time_period is not None:
-        df = df.where(df.date.isin(time_period))
-
-    # Count number of occurences for each page in the considered time period
-    df = df.join(df.groupBy('page_id').agg(count('*').alias('nb_occurrences')), 'page_id').cache()
-    if nb_dates is None:
-        nb_dates = df.select('date').distinct().count()
-
-    # Extract pages which are present during the entire time period and aggregate several metrics
-    df_stable = df.where(df.nb_occurrences == nb_dates).join(df.select('date', 'page_id'), on=['date', 'page_id'])
-    if aggregate:
-        df_stable = df_stable.sort(asc('date')).groupBy('page_id').agg(
-            # collect_list('div').alias('div_sorted_list'),\
-            avg('div').alias('avg_div'), \
-            stddev('div').alias('std_div'), \
-            avg('tot_count_views').alias('avg_pageviews'), \
-            stddev('tot_count_views').alias('std_pageviews'), \
-            avg('rank').alias('avg_rank'), \
-            stddev('rank').alias('std_rank'))
-    return df_stable
 
 
 
