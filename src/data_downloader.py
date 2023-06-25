b441ae7e1884481fa87500c394b5ee689d993a05
PhiCtl <philippine.descourtils@epfl.ch>
2023-06-25 15:52:19 +0200

    Clean up, comment and structure

--- src/data_downloader.py
+++ src/data_downloader.py
@@ -68,12 +68,12 @@ def main():
                         help='year',
                         nargs='+',
                         type=int,
-                        default=[2021, 2022])
+                        default=[2015,2016,2017,2018,2019,2020,2021,2022])
     parser.add_argument('--m',
                         help='months',
                         nargs='+',
                         type=int,
-                        default=[1,2,3,4])
+                        default=[1,2,3,4,5,6,7,8,9,10,11,12])
     parser.add_argument('--sp',
                         help='save path',
                         type=str,
