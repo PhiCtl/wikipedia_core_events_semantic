cf1679e549cdd99aad32c6690aef914f4173eaff
PhiCtl <philippine.descourtils@epfl.ch>
2023-06-25 15:44:09 +0200

    Unused

--- src/_init_.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append('/home/descourt/wikipedia_core_events_semantic')
-
-import ranking_helpers as ranking_helpers
-import topics_analysis as topics_analysis
-import pages_groups_extraction as pages_groups_extraction
-import data_aggregation as data_aggregation
