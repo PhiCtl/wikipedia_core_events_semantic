0b42f76a4f74c5db61eab0386b7cb141b4c75155
PhiCtl <philippine.descourtils@epfl.ch>
2023-06-25 15:58:40 +0200

    French nodes

--- src/data_enrich_processing.py
+++ src/data_enrich_processing.py
@@ -159,4 +159,4 @@ if __name__ == '__main__':
     # create the context
     sc = spark.sparkContext
 
-    parse_metadata(path_in='/scratch/descourt/metadata/akhils_data/wiki_nodes_2022-11', project='fr')
+    parse_metadata(path_in='/scratch/descourt/metadata/akhils_data/wiki_nodes_2022-11.parquet', project='fr')
